b56ddfc5f4ac02cecbc2ff6fad0e76290bb75d20
kaphillips <phillips.katherine@epa.gov>
2022-10-18 16:26:36 -0500

    added r files

---
+++ load_model.R
@@ -0,0 +1,10 @@
+load_model <- function(path,harmonized_use){
+    if (harmonized_use == "all") {
+        ## Load all models
+    } else if () {
+        ## load single model
+    } else {
+        ## No option given, throw an error
+    }
+    return (model)
+}
