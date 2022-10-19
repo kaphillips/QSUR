4e293782912bd4a5ebeb7c5ce5ae0565fe5664f2
kaphillips <phillips.katherine@epa.gov>
2022-10-18 21:43:10 -0500

    Updated files

--- R/load_data.R
+++ R/load_data.R
@@ -1,10 +1,51 @@
-load_model <- function(path,harmonized_use){
+valid_uses <- function(){
+    path <- list.files(path,pattern="*.rds",full.name=F)
+    uses <- unlist(lapply(path,trim))
+    return(uses)
+}
+
+
+load_model <- function(harmonized_use="all"){
+    trim <- function(x){
+        x <- basename(x)
+        ## equiv: x.split("_")[:-2]
+        x <- head(unlist(stringr::str_split(x,"_")),n=-2)
+        return(stringr::str_c(x,collapse="_"))
+    }
+
+    path <- "../data/"
     if (harmonized_use == "all") {
         ## Load all models
-    } else if () {
-        ## load single model
+        path <- list.files(path,pattern="*.rds",full.name=T)
+        uses <- unlist(lapply(path,trim))
+        model <- setNames(as.list(path),uses)
+        model <- lapply(model,readRDS)
     } else {
-        ## No option given, throw an error
+        ## load single model
+        model <- Sys.glob(file.path(path,stringr::str_glue("{harmonized_use}*.rds")))
+        if (length(model) == 0){
+            ## No valid use given, throw an error
+            stop(stringr::str_glue("Unknown use {harmonized_use}. Use `valid_uses` for list of uses with available QSUR model."))
+        } else{
+            # Read in single model
+            model <- readRDS(model)
+        }
     }
     return (model)
+}
+
+
+
+read_toxprints <- function(io,source,chemical_id="chemical_id"){
+    if (source == "chemotyper") {
+        df <- readr::read_tsv(io)
+        df <- dplyr::rename(df,{{chemical_id}}:="M_NAME")
+    } else if (source == "ccd") {
+        df <- readr::read_csv(io)
+        df <- dplyr::rename(df,{{chemical_id}}:="INPUT")
+    }
+    ## The models were built using standard data.frames, but tibbles don't
+    ## conver the column names, so force it
+    colnames(df) <- colnames(data.frame(df))
+    return(df)
 }
--- R/run_models.R
+++ R/run_models.R
@@ -1,36 +1,28 @@
-read_toxprints <- function(source_type,){
-    if (source_type == "chemotyper"){
-
-    } else if (source_typer == 'ccd'){
-        tb <-
-    }
-}
-
-load_model <- function(path,harmonized_use){
-    if (harmonized_use == "all") {
-        ## Load all models
-    } else if () {
-        ## load single model
-    } else {
-        ## No option given, throw an error
-    }
-    return (model)
-}
-
-predict_model <- function(model,X){
-    return (X_pred)
+predict_model <- function(model,df,chemical_id='chemical_id'){
+    df_pred <- randomForest:::predict.randomForest(model,df,type='prob')
+    df_pred <- tibble(as.data.frame(cbind(df[[chemical_id]],preds[,2])))
+    colnames(df_pred) <- c("chemical_id",'probability')
+    df_pred <- df_pred %>% mutate(across(probability,as.double))
+    return (df_pred)
 }
 
 
-in_domain <- function(model,X_pred){
 
-}
-
-workflow <- function(model,X){
-    if (model = NULL) {
-     model <- load_model(path,harmonized_use)
-    } else {
-        preds <- predict_model(model,X)
-        ad <- in_domain(model,X_pred)
-    }
-}
+# predict_model_list <- function(model,df){
+#
+# }
+#
+#
+#
+# in_domain <- function(model,X_pred){
+#
+# }
+#
+# workflow <- function(model,X){
+#     if (model = NULL) {
+#      model <- load_model(path,harmonized_use)
+#     } else {
+#         preds <- predict_model(model,X)
+#         ad <- in_domain(model,X_pred)
+#     }
+# }
